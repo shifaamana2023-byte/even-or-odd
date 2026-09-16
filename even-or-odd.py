a39c87c79f731715969026e6c6f57e9bb6ae4be2
shifaamana2023-byte <shifaamana2023@gmail.com>
2026-09-16 09:37:31 +0530

    Update even-or-odd.py

--- even-or-odd.py
+++ even-or-odd.py
@@ -1,5 +1,11 @@
-num=int(input("enter a number"))
-if num%2==0:
-print("even number")
-else:
-print("odd number")
+
+try:
+    num = int(input("Enter a number: "))  
+
+    if num % 2 == 0:
+        print("Even number")
+    else:
+        print("Odd number")
+
+except ValueError:
+    print("Invalid input! Please enter an integer.")
